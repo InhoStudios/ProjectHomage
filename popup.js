9ab89be99a8fac491b3587b146bbfbb7a06132c9
Andy Zhao <andyshenproper@gmail.com>
2019-09-14 10:37:15 -0400

    Make android proj

--- popup.js
+++
@@ -1,9 +0,0 @@
-document.body.contentEditable = true;
-
-document.addEventListener('click', handler, true);
-
-function handler(e){
-    if(document.body.spellcheck) return;
-    e.stopPropagation();
-    e.preventDefault();
-}
